e526f9c9d0485e0f775324724996e47132443b2a
jsramirezg2 <jsramirezg2@gmail.com>
2026-03-12 15:19:46 -0500

    Refactor matrix initialization and multiplication logic for clarity and correctness

--- mmm_implementation-original.c
+++ mmm_implementation-original.c
@@ -20,19 +20,17 @@ int main(){
         
         // Start modifying here
 
-
-
         for(int i = 0; i < size; i++){
             for(int j = 0; j < size; j++){
-                A[i][j] = 1 + ((double)rand() / RAND_MAX) * MAX_NUM; 
-                B[i][j] = 1 + ((double)rand() / RAND_MAX) * MAX_NUM; 
-                C[i][j] = 0;
+                A[j][i] = 1 + ((double)rand() / RAND_MAX) * MAX_NUM; 
+                B[j][i] = 1 + ((double)rand() / RAND_MAX) * MAX_NUM; 
+                C[j][i] = 0;
             }
         }
-
-        for(int i = 0; i < size; i++){
+        
+        for(int j = 0; j < size; j++){
             for(int k = 0; k < size; k++){
-                for(int j = 0; j < size; j++){
+                for(int i = 0; i < size; i++){
                     C[i][j] += A[i][k] * B[k][j];
                 }
             }
